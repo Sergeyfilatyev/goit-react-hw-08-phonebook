5e2f410b3a19c0873f522375b99e7f8ad4aff781
Sergey Filatyev <filatevsergey2407@gmail.com>
2023-01-11 12:34:00 +0200

    in progress

--- src/redux/contacts/contacts-actions.js
+++ src/redux/contacts/contacts-actions.js
@@ -1,13 +1,16 @@
-import { ADD_CONTACT, DELETE_CONTACT } from './contacts-types';
-export const addContact = contact => {
-  return {
-    type: ADD_CONTACT,
-    payload: contact,
-  };
-};
-export const deleteContact = id => {
-  return {
-    type: DELETE_CONTACT,
-    payload: id,
-  };
-};
+// import { ADD_CONTACT, DELETE_CONTACT } from './contacts-types';
+// export const addContact = contact => {
+//   return {
+//     type: ADD_CONTACT,
+//     payload: contact,
+//   };
+// };
+// export const deleteContact = id => {
+//   return {
+//     type: DELETE_CONTACT,
+//     payload: id,
+//   };
+// };
+import { createAction } from '@reduxjs/toolkit';
+export const addContact = createAction('contacts/add');
+export const deleteContact = createAction('contacts/delete');
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,13 @@
-import { createStore, combineReducers } from 'redux';
-import { devToolsEnhancer } from '@redux-devtools/extension';
+// import { createStore, combineReducers } from 'redux';
+// import { devToolsEnhancer } from '@redux-devtools/extension';
+// import { contactsReducer } from './contacts/contacts-reducers';
+// const enchancer = devToolsEnhancer();
+// const rootReducer = combineReducers({ contacts: contactsReducer });
+// export const store = createStore(rootReducer, enchancer);
+import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contacts/contacts-reducers';
-const enchancer = devToolsEnhancer();
-const rootReducer = combineReducers({ contacts: contactsReducer });
-export const store = createStore(rootReducer, enchancer);
+export const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+  },
+});
