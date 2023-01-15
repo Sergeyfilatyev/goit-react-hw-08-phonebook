1fba2240c63cebf83bda64934333705bc27f2e60
Sergey Filatyev <filatevsergey2407@gmail.com>
2023-01-15 17:17:55 +0200

    start-test-back

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,11 @@ import { Filter } from './Filter/Filter';
 import s from './App.module.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
 export default function App() {
+  axios.defaults.baseURL = 'https://63c40c4aa9085635753089ee.mockapi.io/';
+  const fetchContacts = () => axios('contacts').then(r => console.log(r.data));
+  fetchContacts();
   return (
     <div className={s.box}>
       <h1 className={s.title}>Phonebook</h1>
