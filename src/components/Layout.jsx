d62e65f5c594348d176ae12c83c3947523a1e16d
Sergey Filatyev <filatevsergey2407@gmail.com>
2023-01-27 15:29:18 +0200

    styles-start

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,18 +4,27 @@ import { Navigation } from 'components/Navigation';
 import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 import { selectToken } from 'redux/auth/auth-selectors';
+import { Box, Center } from '@chakra-ui/react';
 export const Layout = () => {
   const token = useSelector(selectToken);
   return (
     <>
       <header>
-        <Navigation />
-        {token ? <AuthUserMenu /> : <AuthNavigation />}
+        <Box bg="#6b7c2c" w="100%" color="#ffffff" gap="10px">
+          <Navigation />
+          {token ? <AuthUserMenu /> : <AuthNavigation />}
+        </Box>
       </header>
       <main>
-        <Outlet />
+        <Box bg="#98b33a" w="100%" color="#ffffff">
+          <Outlet />
+        </Box>
       </main>
-      <footer>Phonebook by SergeyFilatyev </footer>
+      <footer>
+        <Box bg="#6b7c2c" w="100%" color="#ffffff">
+          @Phonebook created by SergeyFilatyev
+        </Box>
+      </footer>
     </>
   );
 };
