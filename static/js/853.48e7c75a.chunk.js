e6c3f646a7160166580ad90e5a8372c643a44814
Sergeyfilatyev <102666091+Sergeyfilatyev@users.noreply.github.com>
2023-01-29 20:11:22 +0000

    Deploying to gh-pages from @ Sergeyfilatyev/goit-react-hw-08-phonebook@f969859ffe3bb6280649b33c870af1127eded99f 🚀

---
+++ static/js/853.48e7c75a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[853],{3853:function(t,o,n){n.d(o,{Imn:function(){return r}});var e=n(9983);function r(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"path",attr:{d:"M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"}},{tag:"polyline",attr:{points:"22,6 12,13 2,6"}}]})(t)}}}]);
+//# sourceMappingURL=853.48e7c75a.chunk.js.map
